322e328a9f82262e3d40ea2fc4483639fea9c045
Puskal Kafle <puskal@Puskal-2.local>
2015-07-21 22:42:28 +0545

    a

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
-var scotchApp = angular.module('scotchApp', ['ngRoute']);
+// script.js
+
+// create the module and name it pkApp
+// also include ngRoute for all our routing needs
+var pkApp = angular.module('pkApp', ['ngRoute']);
 
 // configure our routes
-scotchApp.config(function($routeProvider) {
+pkApp.config(function($routeProvider) {
     $routeProvider
 
         // route for the home page
         .when('/', {
-            templateUrl : '../pages/home.html',
+            templateUrl : 'pages/home.html',
             controller  : 'mainController'
         })
 
         // route for the about page
-        .when('/about', {
-            templateUrl : '../pages/about.html',
-            controller  : 'aboutController'
+        .when('/projects', {
+            templateUrl : 'pages/projects.html',
+            controller  : 'projectsController'
         })
 
         // route for the contact page
         .when('/contact', {
-            templateUrl : '../pages/contact.html',
+            templateUrl : 'pages/contact.html',
             controller  : 'contactController'
+        })
+        .when('/expertise', {
+            templateUrl : 'pages/expertise.html',
+            controller  : 'expertiseController'
         });
 });
 
 // create the controller and inject Angular's $scope
-scotchApp.controller('mainController', function($scope) {
+pkApp.controller('mainController', function($scope) {
     // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
+    $scope.message = 'Everyone come and see how good I look! Home Page';
 });
 
-scotchApp.controller('aboutController', function($scope) {
-    $scope.message = 'Look! I am an about page.';
+pkApp.controller('projectsController', function($scope) {
+    $scope.message = 'Look! I am an Projects page.';
 });
 
-scotchApp.controller('contactController', function($scope) {
-    $scope.message = 'Contact us! JK. This is just a demo.';
+pkApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a Contact Page.';
+});
+
+pkApp.controller('expertiseController', function($scope) {
+    $scope.message = 'Expertise! JK. This is just a demo.';
 });
